c0b1bdbd808c1924988f488e628ab61cf23080b2
NishaRaut <nisharaut84@gmail.com>
2020-02-25 12:33:04 +0530

    Added: Calculate part time wages

--- EmpWages.java
+++ EmpWages.java
@@ -1,14 +1,14 @@
 class EmpWages {
-
+	//constants
+	public static final int wagePerHr = 20;
+   public static final int fullDayHr = 8;
+   public static final int partTimeHr = 4;
  public static void main(String args[]){
-	int wagePerHr = 20;
-	int fullDayHr = 8;
-	
 	long random = Math.round( Math.random() )  ;
 	if( random == 1 ){
 		System.out.println("Employee is present..");
-		long dailyWages = wagePerHr * fullDayHr;
-		System.out.println("Daily Employee Wage = "+dailyWages);
+		long dailyWages = wagePerHr * partTimeHr;
+		System.out.println("Part Time Employee Wage = "+dailyWages);
 	}
 	else
 	{
