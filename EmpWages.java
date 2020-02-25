61f46ac8c77c979de0ca7aeb58f47bc09f8e8997
NishaRaut <nisharaut84@gmail.com>
2020-02-25 11:37:41 +0530

    Added: Check Attendance

--- EmpWages.java
+++ EmpWages.java
@@ -1,5 +1,14 @@
-class EmpWages{
- public static void main(String args[]){  
-System.out.println("welcome to employee wages");
-}
+class EmpWages {
+
+ public static void main(String args[]){
+	System.out.println("welcome to employee wages");
+	long random = Math.round( Math.random() )  ;
+	if( random == 1 ){
+		System.out.println("Employee is present..");
+	}
+	else
+	{
+	System.out.println("Employee is absent!!");
+	}
+ }
 }
