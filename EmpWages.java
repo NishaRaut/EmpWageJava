eafa2900ba4b247071dd641bc98e85329d923b5e
NishaRaut <nisharaut84@gmail.com>
2020-02-25 13:15:18 +0530

    Added: calculate wages using switch case

--- EmpWages.java
+++ EmpWages.java
@@ -3,16 +3,27 @@ class EmpWages {
 	public static final int wagePerHr = 20;
    public static final int fullDayHr = 8;
    public static final int partTimeHr = 4;
+
  public static void main(String args[]){
-	long random = Math.round( Math.random() )  ;
-	if( random == 1 ){
-		System.out.println("Employee is present..");
-		long dailyWages = wagePerHr * partTimeHr;
-		System.out.println("Part Time Employee Wage = "+dailyWages);
+	int empHr = 0;
+	int random = (int)(Math.floor( Math.random()*10)%3 );
+
+	switch ( (int)random ){
+	case 1:
+		System.out.println("Employee present full time: ");
+		empHr = fullDayHr;
+		break;
+	case 2:
+		System.out.println("Employee present part time: ");
+		empHr = partTimeHr;
+		break;
+	default :
+		System.out.println("Employee is Absent!!");
+		empHr = 0 ;
+		break;
 	}
-	else
-	{
-		System.out.println("Employee is absent!!");
-	}
+
+	int dailyEmpWages = wagePerHr * empHr;
+	System.out.println("Daily employee wages: "+dailyEmpWages);
  }
 }
