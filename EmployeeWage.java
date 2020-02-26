947299417b052fcbef02c9b8e6435bc476ccdd08
NishaRaut <nisharaut84@gmail.com>
2020-02-26 17:14:38 +0530

    Added: calculate emp wages for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,6 @@
 class EmpWages {
 	//constants
-	static final int  fullDayHr = 8;
+	static final int  fulDayHr = 8;
 	static final int partTimeHr = 4;
 
 	//variabels
@@ -19,30 +19,36 @@ class EmpWages {
 
 public class EmployeeWage {
 
- public int calculateDailyWages(EmployeeWage emp){
-	int empHr = 0;
-	int num=0;
-	int day=0;
-	EmpWages companyArray[] = new EmpWages[5];
-  
-	while(day<=emp.numOfWorkingDays)
-	int random = (int)(Math.floor( Math.random()*10)%3 );
-	day++
-	switch ( (int)random ){
-			case 1:
-			empHr = fullDayHr;
-			break;
-		case 2:
-			empHr = partTimeHr;
-			break;
-		default :
-			empHr = 0 ;
-			break;
-		}
-		int dailyEmpWages = empHr * emp.wagePerHr; 
+	EmpWages companyArray[] = new EmpWages[2];
+	int  num=0;
+	int empHr=0;
+
+   public void  calculateWages(){
+	
+		for(int i=0;i<companyArray.length;i++)
+		{
+			int day=0;
+			System.out.println(companyArray[i].companyName);
+			while(day<=companyArray[i].numOfWorkingDays && empHr<=companyArray[i].monthWorkingHrs)
+			{
+				int random = (int)(Math.floor( Math.random()*10)%3 );
+				day++;
+				switch ( (int)random ){
+				case 1:
+					empHr = 8;
+					break;
+				case 2:
+					empHr = 4;
+					break;
+				default :
+					empHr = 0 ;
+					break;
+				}
+		int dailyEmpWages = empHr * companyArray[i].wagePerHr; 
 		System.out.println(">>>>>>>"+dailyEmpWages);
-	return dailyEmpWages;
+		}
 	}
+}
 
    public void addCompany(int wage_Per_Hr,int month_Working_Hrs,int numof_Working_Days,String company_Name){
 	 companyArray[num++] = new EmpWages(wage_Per_Hr, month_Working_Hrs, numof_Working_Days, company_Name);
@@ -50,7 +56,12 @@ public class EmployeeWage {
 
 	public static void main(String args[]){
 		EmployeeWage  empWage=new EmployeeWage();
-		empWage.addCompany(20,2,2,"Dmart");	
-		empWage.calculateWage(empWage);
+		empWage.addCompany(20,100,20,"Dmart");
+		empWage.addCompany(20,100,10,"Relince");	
+		empWage.calculateWages();
 	}
 }
+
+
+
+
